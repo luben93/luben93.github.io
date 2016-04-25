7f4803f297075a2376b37173b6ceb48e4a74a7c2
lucas <luben93@users.noreply.github.com>
2016-04-25 13:23:39 +0200

    Update deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -10,4 +10,6 @@ end
 
 res = JSON.parse(open("https://api.github.com/repos/luben93/luben93.github.io/commits").read)
 
-client.update("i wrote about #{res[0]['commit']['message']}, check it out check, my blog is at https://luben93.github.io #blog")
+unless res[0]['commit']['message'] == res[0]['commit']['message']  do
+  client.update("i wrote about #{res[0]['commit']['message']}, check it out check, my blog is at https://luben93.github.io #blog")
+end
