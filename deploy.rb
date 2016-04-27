9dda1346224a18465534ba0d6a5d7a01667d905a
lucas <luben93@users.noreply.github.com>
2016-04-27 11:10:14 +0200

    Update deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -11,5 +11,5 @@ end
 res = JSON.parse(open("https://api.github.com/repos/luben93/luben93.github.io/commits").read)
 
 if res[0]['commit']['message'] =~ /^post/  then
-  client.update("i wrote #{res[0]['commit']['message']}, check it out, my blog is at https://luben93.github.io #blog")
+  client.update("i wrote a #{res[0]['commit']['message']}, check it out, my blog is at https://luben93.github.io #blog")
 end
